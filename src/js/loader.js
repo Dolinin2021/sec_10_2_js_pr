1245fa169d5b26955df479eba26980b30fd2b4e8
Dolinin2021 <programmistdolinin@mail.ru>
2023-03-15 21:08:34 +0600

    fix

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,10 +1,13 @@
 import read from './reader';
 import json from './parser';
+import GameSaving from './saving';
 
 export default class GameSavingLoader {
   static async load() {
     const data = await read();
     const value = await json(data);
-    return value;
+    const res = JSON.parse(value);
+    const instance = new GameSaving(res.id, res.created, res.userInfo);
+    return instance;
   }
 }
